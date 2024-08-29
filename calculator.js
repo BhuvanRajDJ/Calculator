89c4003d7d0d1e225868f3d5de74408ef1ea7ca3
BhuvanRajDJ <133094255+BhuvanRajDJ@users.noreply.github.com>
2024-08-29 18:36:59 +0530

    calculator

--- calculator.js
+++ calculator.js
@@ -1,5 +1,9 @@
 const display = document.getElementById("display");
+const historyElement = document.getElementById("history");
+const historyList = document.createElement("ul");
+historyElement.appendChild(historyList);
 
+let history = [];
 function addelementtodisplay(input) {
   display.value += input;
 }
@@ -11,8 +15,8 @@ function Calculate(input) {
 function Calculate1() {
   try {
     const result = eval(display.value);
+    addToHistory(display.value, result);
     display.value = result;
-    historyArray.push(result);
   } catch {
     display.value = "Error";
   }
@@ -25,3 +29,28 @@ function clearDisplay() {
 function deleteprevelement() {
   display.value = display.value.slice(0, -1);
 }
+
+function addToHistory(expression, result) {
+  const historyItem = `${expression} = ${result}`;
+  history.push(historyItem);
+
+  const li = document.createElement("li");
+  li.textContent = historyItem;
+  historyList.appendChild(li);
+}
+
+function clearHistory() {
+  history = [];
+  historyList.innerHTML = "";
+}
+
+document.querySelector(".history").addEventListener("click", () => {
+  if (
+    historyElement.style.display === "none" ||
+    historyElement.style.display === ""
+  ) {
+    historyElement.style.display = "block";
+  } else {
+    historyElement.style.display = "none";
+  }
+});
